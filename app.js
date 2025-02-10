0826b908ac03214700ed79fd2939b4e77e6cac3b
Matthew Beckwith <mbeckwith@morganautogroup.com>
2025-02-10 13:04:14 -0500

    connected to ssh and pushed cosmetic changes

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const { createApp, ref, computed } = Vue;
 createApp({
     data(){
         return {
-            isModelID: ref(false),
-            isModelBuzz: ref(false),
+            modelChoice: ref("gen"),
             invoiceAmt: ref(null),
-            modelChoice: ref('gen'),
             msrpAmt: ref(null),
             baseMsrpAmt: ref(null),
             destinationAmt: ref(null),
@@ -20,13 +18,13 @@ createApp({
                 return (0).toFixed(2)
             }),
             getHoldback: computed(() => {
-                if(this.modelChoice === 'id4') {
+                if(this.modelChoice === "id4") {
                     return (this.baseMsrpAmt * 0.048).toFixed(2)
                 }
                 return Math.round(this.getTotal * 0.02).toFixed(2)
             }),
             getOptionsHB: computed(() => {
-                if(this.modelChoice === 'id4') {
+                if(this.modelChoice === "id4") {
                     if(this.optionsAmt > 0) {
                         return Math.round(this.optionsAmt * 0.02).toFixed(2)
                     }
@@ -35,7 +33,7 @@ createApp({
                 return (0).toFixed(2)
             }),
             getPaintHB: computed(() => {
-                if(this.modelChoice === 'id4') {
+                if(this.modelChoice === "id4") {
                     if(this.paintAmt > 0) {
                         return Math.round(this.paintAmt * 0.078).toFixed(2)
                     }
@@ -44,7 +42,7 @@ createApp({
                 return (0).toFixed(2)
             }),
             getTotalHB: computed(() => {
-                if(this.modelChoice === 'id4') {
+                if(this.modelChoice === "id4") {
                     const hb = Number(this.getHoldback)
                     const opt = Number(this.getOptionsHB)
                     const pnt = Number(this.getPaintHB)
@@ -59,7 +57,7 @@ createApp({
                 return Math.round(this.baseMsrpAmt * 0.015).toFixed(2)
             }),
             getIDM: computed(() => {
-                if(this.modelChoice === 'id4') {
+                if(this.modelChoice === "id4") {
                     return Math.round(this.baseMsrpAmt * 0.02).toFixed(2)
                 }
                 return Math.round(this.baseMsrpAmt * 0.008).toFixed(2)
@@ -71,9 +69,7 @@ createApp({
                 return Math.round(this.baseMsrpAmt * 0.019).toFixed(2)
             }),
             clearForm: () => {
-                this.isModelID = false
-                this.isModelBuzz = false
-                this.modelChoice = 'gen'
+                this.modelChoice = "gen"
                 this.invoiceAmt = null
                 this.msrpAmt = null
                 this.baseMsrpAmt = null
